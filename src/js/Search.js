d6cb97028dd8e55cc515809d6bfa094c93ca0599
JeongHui-seong <jgmltjd99@naver.com>
2025-06-02 11:07:34 +0900

    search

--- src/js/Search.js
+++ src/js/Search.js
@@ -1,14 +1,45 @@
+import DB from "./supabase.js";
+
 export default class Search {
     constructor(target) {
         this.target = target;
+        this.db = new DB();
+    }
+    debounce(func, delay){
+        let timeout;
+        return function (...args) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, args), delay);
+        };
+    }
+    setEventListener(){
+        const inpSearch = document.getElementById("inp_search");
+        inpSearch.addEventListener("input", this.debounce((e)=>{
+            console.log(e.target.value.trim())
+        },300));
     }
     template() {
-        return `검색(만드는중)`
+        return `
+        <div class="container">
+                <input type="text" id="inp_search" placeholder="아이디 또는 이름을 검색해주세요.">
+            <div class="searched_box">
+                <ul>
+                    <li class="searched_list">
+                        <a href="#/mypage/">
+                            <p class="name">이름</p>
+                            <p class="id">아이디</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        `
     }
     render(target) {
         const page = window.location.hash;
         if (page == "#/search"){
             target.innerHTML = this.template();
+            this.setEventListener();
 
             const $nav = document.getElementById("nav");
             const $navTagA = document.querySelector("#nav li:nth-of-type(3) a");
